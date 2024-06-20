84a0a9b9ef912a9d03b89ff1422f86dd9a1d0c83
ClaudioRMalvino <claudiormal@gmail.com>
2024-06-19 20:59:06 -0400

    Updated main.py to include word_count()

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
 def main():
-    """Reads the contents of Frankenstein from path."""
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = word_count(text)
+    num_char = char_count(text)
+    print(f"{num_words} words found in the document")
+
+def num_char(text):
+    """Returns a count of the number of character instances
+        from the provided text.  
+
+    Args:
+        text (string): The context of a text document.
+
+    Returns:
+        dict: returns a dictionary key = char, 
+            value = and int value of instances.
+    """
     
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    
+    
+def word_count(text):
+    """Returns a word count from the provided text.  
 
-if __name__ == '__main__':
-    main()
+    Args:
+        text (string): The context of a text document.
 
-   
+    Returns:
+        int: returns the word count.
+    """
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    """Fetches the contents of a text file located at path.
+
+    Args:
+        path (string): relative path of the text.
+
+    Returns:
+        string: returns the content of file in path.
+    """
+    with open(path) as f:
+        return f.read()
+
+
+main()
